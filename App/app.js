b122101d74798cd5e6b0bfd12165d270ed49b609
sbutt3716 <sbutt3716@gmail.com>
2021-01-29 18:56:47 +0500

    final update

--- App/app.js
+++ App/app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express.Router();
 //helmet and morgan is third-party mildware
 const morgan = require("morgan");
-//custom middleware
-// const logger = require("../middleware/logger");
 const winston = require("winston");
 require("../middleware/logger")();
 require("../middleware/error");
@@ -14,7 +12,5 @@ app.use("/api/", routes_folder);
 
 //assigning Public Folder to our app
 app.use(express.static("public"));
-//assigning custom middleware function to our app
-// app.use(logger);
 
 module.exports = app;
